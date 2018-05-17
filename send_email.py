4c813d5ecceeebb84c91aa27099c25c417ef0067
A-fish-in-Lake-Baikal <38976160+A-fish-in-Lake-Baikal@users.noreply.github.com>
2018-05-17 17:13:24 +0800

    send_email
    发送邮件

--- send_email.py
+++ send_email.py
@@ -15,13 +15,13 @@ with open (r'E:\python\RC2.3.3\test_report\2018-05-17-14_30_42HTMLtemplate.html'
 
 from_addr = '3536046934@qq.com'
 password = 'ilxhssalsnvpchjg'
-to_addr = '1627967707@qq.com'
+to_addr = '3409256447@qq.com'
 smtp_server = 'smtp.qq.com'
 
-msg = MIMEText(text,'html', 'utf-8')
-msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
+msg = MIMEText("马维畅是你大爷",'plain', 'utf-8')
+msg['From'] = _format_addr('你大爷的来信 <%s>' % from_addr)
 msg['To'] = _format_addr('管理员 <%s>' % to_addr)
-msg['Subject'] = Header('这是标题', 'utf-8').encode()
+msg['Subject'] = Header('马大爷', 'utf-8').encode()
 
 server = smtplib.SMTP(smtp_server, 25)
 server.set_debuglevel(1)
