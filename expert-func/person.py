047185d5521ca510d724d0b6715c078753735618
A-fish-in-Lake-Baikal <38976160+A-fish-in-Lake-Baikal@users.noreply.github.com>
2018-05-29 21:45:34 +0800

    类练习
    类练习

--- expert-func/person.py
+++ expert-func/person.py
@@ -9,12 +9,25 @@ class person(object):
         return self.name.split()[0]
     def giveraise(self,percent):
         self.pay = int(self.pay*(1+percent))
+    def __str__(self):
+        return '[person:%s,%s]' %(self.name,self.pay)
 
+class manager(person):
+        def giveraise(self,percent,bonus=.10):
+            person.giveraise(self,percent+bonus)
 if __name__=='__main__':
     b = person('Bob smitch')
     s = person('sue jones','It',5000)
-    print(b.name,b.job,b.pay)
-    print(s.name,s.job,s.pay)
+    c = manager('tomsen wils','banzhuan',6000)
+    print(b)
+    print(s)
+    print(c)
+    c.giveraise(.10)
+    print(c.pay)
     print(b.first_name(),s.first_name())
     s.giveraise(0.1)
     print(s.pay)
+    print("-----all three-----")
+    for object in (b,s,c):
+        object.giveraise(.10)
+        print(object)
