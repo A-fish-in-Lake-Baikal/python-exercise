4be112ef2b9655938df9a95c53da113409ebcfb7
A-fish-in-Lake-Baikal <38976160+A-fish-in-Lake-Baikal@users.noreply.github.com>
2018-05-29 20:53:38 +0800

    sendemail
    sendemail

--- WEB/send_email.py
+++ WEB/send_email.py
@@ -5,27 +5,30 @@ from email.header import Header
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
 import smtplib
+def send_maile():
+    def _format_addr(s):
+        name, addr = parseaddr(s)
+        return formataddr((Header(name, 'utf-8').encode(), addr))
 
-def _format_addr(s):
-    name, addr = parseaddr(s)
-    return formataddr((Header(name, 'utf-8').encode(), addr))
+    with open (r'html_1.html','rb') as file:
+        text=file.read()
+        print('-------------------------------------------%s' %text)
+    from_addr = '3536046934@qq.com'
+    password = 'ilxhssalsnvpchjg'
+    to_addr = 'mawc@parasaga.com'
+    smtp_server = 'smtp.qq.com'
 
-with open (r'E:\python\RC2.3.3\test_report\2018-05-22-16_18_25HTMLtemplate.html','rb') as file:
-    text=file.read()
-    print('-------------------------------------------%s' %text)
-from_addr = '3536046934@qq.com'
-password = 'ilxhssalsnvpchjg'
-to_addr = 'mawc@parasaga.com'
-smtp_server = 'smtp.qq.com'
+    msg = MIMEText(text,'html', 'utf-8')
+    msg['From'] = _format_addr('tester <%s>' % from_addr)
+    msg['To'] = _format_addr('管理员 <%s>' % to_addr)
+    msg['Subject'] = Header('自动化测试报告', 'utf-8').encode()
 
-msg = MIMEText(text,'html', 'utf-8')
-msg['From'] = _format_addr('tester <%s>' % from_addr)
-msg['To'] = _format_addr('管理员 <%s>' % to_addr)
-msg['Subject'] = Header('自动化测试报告', 'utf-8').encode()
+    server = smtplib.SMTP(smtp_server, 25)
+    server.set_debuglevel(1)
+    server.starttls()
+    server.login(from_addr, password)
+    server.sendmail(from_addr, [to_addr], msg.as_string())
+    server.quit()
 
-server = smtplib.SMTP(smtp_server, 25)
-server.set_debuglevel(1)
-server.starttls()
-server.login(from_addr, password)
-server.sendmail(from_addr, [to_addr], msg.as_string())
-server.quit()
+if __name__=='__main__':
+    send_maile()
