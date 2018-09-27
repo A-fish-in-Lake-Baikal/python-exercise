b85d42f69f7783864c59dab424487aa5347803af
A-fish-in-Lake-Baikal <38976160+A-fish-in-Lake-Baikal@users.noreply.github.com>
2018-09-27 21:46:05 +0800

    flask_object
    flask_page

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -7,9 +7,33 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'This is my home page!'
+
 @app.route('/index')
 def index():
-    return """"""
+    return render_template("index.html",title='Home',name='马牛逼')
+
+@app.route('/page')
+def page():
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    contents = [
+        {'zn':'这是一段中文简介','ch':'this is english brief'},
+        {'zn': '这是汉语', 'ch': 'this is english'}
+                ]
+    return render_template("page.html",
+                           title='Home',
+                           user=user,
+                           posts=posts,
+                           contents=contents)
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
